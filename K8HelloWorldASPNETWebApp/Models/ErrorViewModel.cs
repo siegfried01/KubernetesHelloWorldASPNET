63e1410ad421ead56db41f7f90b60e442b2268c6
siegfried01 <sheintze@hotmail.com>
2020-08-21 22:16:27 -0700

    initial

---
+++ K8HelloWorldASPNETWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace KubernetesHelloWorldASPNET.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
